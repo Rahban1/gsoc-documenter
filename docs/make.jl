b416ac7379361a0d3454d4883f9bbc14460abedf
rahban <rahban.ghani2001@gmail.com>
2025-05-23 17:11:43 +0530

    logo and section name changed

--- docs/make.jl
+++ docs/make.jl
@@ -2,16 +2,15 @@ using Documenter
 
 makedocs(
     sitename = "GSoC Documentation",
-    logo = "assets/gsoc_logo.png",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
         canonical = "https://rahban1.github.io/gsoc-documenter/",
         assets = String[],
-        analytics = "UA-XXXXXXXXX-X"
+        analytics = "UA-XXXXXXXXX-X",
     ),
     pages = [
         "Home" => "index.md",
-        "Community-bonding Period" => "community-bonding.md",
+        "Deliverable Summary" => "deliverable-summaries.md",
     ],
     build = "build",
 )
